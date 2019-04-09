f7811adf481fa67f61442be11b54d6da587ac61c
kihara.k <kihara.k2000@gmail.com>
2019-04-09 14:27:20 +0900

    ランキング

---
+++ Destroy/Assets/CharaData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharaData : MonoBehaviour
+{
+    static int score;
+    public static int GetScore()
+    {
+        return score;
+    }
+    
+}
