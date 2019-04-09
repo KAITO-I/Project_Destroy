592f5a1864b5d3dc2583b4e714da7d819e2a69c2
kihara.k <kihara.k2000@gmail.com>
2019-04-09 13:37:28 +0900

    視界内のオブジェクト透明化

---
+++ Destroy/Assets/Camera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Transparent(other.gameObject);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        UnTransparent(other.gameObject);
+    }
+    void Transparent(GameObject go)
+    {
+        MeshRenderer mesh = go.GetComponent<MeshRenderer>();
+        mesh.material.color = new Color(mesh.material.color.r, mesh.material.color.g, mesh.material.color.b, 0.5f);
+    }
+    void UnTransparent(GameObject go)
+    {
+        MeshRenderer mesh = go.GetComponent<MeshRenderer>();
+        mesh.material.color = new Color(mesh.material.color.r, mesh.material.color.g, mesh.material.color.b, 1f);
+    }
+}
