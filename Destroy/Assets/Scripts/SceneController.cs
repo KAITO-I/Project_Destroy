97db1773ea2d496ef40976d92ff8464b076d6b60
KAITO-I <inafuku.tnw@gmail.com>
2019-04-09 18:55:48 +0900

    フェードイン、フェードアウトの処理実装

---
+++ Destroy/Assets/Scripts/SceneController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    private static SceneController instance;
+
+    public static SceneController Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = (SceneController) FindObjectOfType(typeof(SceneController));
+                if (instance == null) Debug.LogError(typeof(SceneController) + "がシーン上にありません！");
+            }
+            return instance;
+        }
+    }
+
+    private AsyncOperation async;
+    private float interval = 2f;
+
+    private float fadeAlpha = 0f;
+    private bool isFading = false;
+    private Color fadeColor = Color.black;
+
+    private void Awake()
+    {
+        if (this != Instance)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void OnGUI()
+    {
+        //フェード
+        if (this.isFading)
+        {
+            //色と透明度を更新して白テクスチャを描画
+            this.fadeColor.a = this.fadeAlpha;
+            GUI.color = this.fadeColor;
+            GUI.DrawTexture(new Rect(0f, 0f, Screen.width, Screen.height), Texture2D.whiteTexture);
+        }
+    }
+
+    public void Load(string scene)
+    {
+        StartCoroutine(LoadScene(scene));
+    }
+
+    private IEnumerator LoadScene(string scene)
+    {
+        this.isFading = true;
+
+        //だんだん暗く
+        float time = 0f;
+        while (time <= interval)
+        {
+            this.fadeAlpha = Mathf.Lerp(0f, 1f, time / interval);
+            time += Time.deltaTime;
+            yield return 0;
+        }
+
+        //シーン切り替え
+        this.async = SceneManager.LoadSceneAsync(scene);
+        while (!async.isDone)
+        {
+            yield return null;
+        }
+
+        //だんだん明るく
+        time = 0f;
+        while (time <= interval)
+        {
+            this.fadeAlpha = Mathf.Lerp(1f, 0f, time / interval);
+            time += Time.deltaTime;
+            yield return 0;
+        }
+
+        this.isFading = false;
+    }
+}
--- Destroy/Assets/Scripts/TitleManager.cs
+++ Destroy/Assets/Scripts/TitleManager.cs
@@ -1,31 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
 
 public class TitleManager : MonoBehaviour
 {
-    private AudioSource audio;
-    [SerializeField] AudioClip[] voices;
-    [SerializeField] TextMeshProUGUI subtitle;
-    [SerializeField] string[] subtitles;
-
     void Start()
     {
-        this.audio = GetComponent<AudioSource>();
-        StartCoroutine(voicePlay());
+        Cursor.visible = false;
+
+        GameObject.Find("Canvas/MainUI/StartButton").GetComponent<Button>().Select();
     }
 
-    private IEnumerator voicePlay()
+    public void OnClickStart()
     {
-        int num = 0;
-        while (num < voices.Length)
-        {
-            audio.clip = voices[num];
-            audio.Play();
-            subtitle.text = subtitles[num];
-            yield return new WaitForSeconds(voices[num].length);
-            num++;
-        }
+        FadeManager.Instance.LoadScene("MainGame", 1f);
+    }
+
+    public void OnClickRanking()
+    {
+
     }
 }
