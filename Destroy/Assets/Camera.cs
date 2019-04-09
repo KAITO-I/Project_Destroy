abad1eaba07c715b8e7fbe6219668f67bb1de5aa
kihara.k <kihara.k2000@gmail.com>
2019-04-09 13:40:35 +0900

    透明化の修正

--- Destroy/Assets/Camera.cs
+++ Destroy/Assets/Camera.cs
@@ -15,11 +15,17 @@ public class Camera : MonoBehaviour
     void Transparent(GameObject go)
     {
         MeshRenderer mesh = go.GetComponent<MeshRenderer>();
-        mesh.material.color = new Color(mesh.material.color.r, mesh.material.color.g, mesh.material.color.b, 0.5f);
+        if(mesh != null)
+        {
+            mesh.material.color = new Color(mesh.material.color.r, mesh.material.color.g, mesh.material.color.b, 0.5f);
+        }
     }
     void UnTransparent(GameObject go)
     {
         MeshRenderer mesh = go.GetComponent<MeshRenderer>();
-        mesh.material.color = new Color(mesh.material.color.r, mesh.material.color.g, mesh.material.color.b, 1f);
+        if (mesh != null)
+        {
+            mesh.material.color = new Color(mesh.material.color.r, mesh.material.color.g, mesh.material.color.b, 1f);
+        }
     }
 }
