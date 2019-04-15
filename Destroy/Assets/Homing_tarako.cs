ac2cd732d1004349d61945be715baedf19b5fe55
Tatsu_K <kubo2019cooljapan@gmail.com>
2019-04-15 11:53:40 +0900

    たらこ移動/アニメ

---
+++ Destroy/Assets/Homing_tarako.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    [RequireComponent(typeof(ThirdPersonCharacter))]
+    // [RequireComponent(typeof(NavMeshAgent))]
+    public class Homing_tarako : MonoBehaviour
+    {
+        public enum EnemyState
+        {
+            Nomal,
+            Chase
+        };
+
+        public EnemyState state;
+        AICharacterControl ai;
+        public GameObject target;
+        public NavMeshAgent agent;
+        public Material Red, Blue;
+        private new Renderer renderer = null;
+
+        public float Speed;
+        public bool Flag = false, Flag2 = false, Flag3 = true;
+        Animator animator;
+
+
+        void Start()
+        {
+            target = GameObject.FindWithTag("Player")
+; renderer = GetComponentInChildren<Renderer>();
+            target = GameObject.FindWithTag("Player");
+            agent = GetComponent<NavMeshAgent>();
+            agent.speed = 1;
+            animator = GetComponent<Animator>();
+            SetState("Nomal");
+            ai = GetComponent<AICharacterControl>();
+        }
+
+        void Update()
+        {
+
+            if (state == EnemyState.Nomal) renderer.material = Blue;
+
+            if (Flag == true && Flag2 == false)
+            {
+                if (Input.GetKeyDown(KeyCode.Space) && state == EnemyState.Nomal)
+                {
+                    Chase();
+                }
+            }
+            if (Flag == true && Flag3 == true)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    Flag3 = false;
+                    GameObject.Find("MainGameManager").GetComponent<ScoreManager>().Scorecalc(gameObject, gameObject.GetComponent<CharaData>().haveItem);
+                }
+            }
+            Get();
+        }
+        void Chase()
+        {
+            SetState("Chase");
+            agent.speed = 0;
+            animator.SetBool("Flag", true);
+            SetState("Chase");
+            agent.SetDestination(this.transform.position);
+            Invoke("Chase_Start", 2.0f);
+        }
+        public void SetState(string Mode)
+        {
+            if (Mode == "Nomal")
+            {
+                state = EnemyState.Nomal;
+
+            }
+            if (Mode == "Chase")
+            {
+                state = EnemyState.Chase;
+            }
+        }
+        public void Get()
+        {
+            if (state == EnemyState.Chase && Flag2 == true)
+            {
+                agent.SetDestination(target.transform.position);
+            }
+        }
+        void Chase_Start()
+        {
+            Flag2 = true;
+            Flag = false;
+            agent.speed = Speed;
+            ai.Null();
+            Get();
+            this.tag = "Enemy";
+            if (state == EnemyState.Chase) renderer.material = Red;
+
+        }
+
+        public void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Crusher")
+            {
+                Flag = true;
+            }
+        }
+        public void OnTriggerExit(Collider other)
+        {
+
+            Flag = false;
+
+        }
+    }
+}
