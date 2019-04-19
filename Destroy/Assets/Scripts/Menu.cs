972a24243924ba05658755e99f67d2fe5a697d8f
Haruta_prog <haruta.prog1999@gmail.com>
2019-04-19 14:22:25 +0900

    ファイル整理など

---
+++ Destroy/Assets/Scripts/Menu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject MenuWindow;
+    void Start()
+    {
+        if(MenuWindow.activeInHierarchy == true)MenuWindow.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Cancelを押すことで画面が止まりMapが表示される
+        if (Input.GetButtonDown("Cancel") && MenuWindow.activeInHierarchy == false)
+        {
+            Time.timeScale = 0;
+            MenuWindow.SetActive(true);
+        }
+        //もう一度押すと解除
+        else if (Input.GetButtonDown("Cancel") && MenuWindow.activeInHierarchy == true)
+        {
+            Time.timeScale = 1;
+            MenuWindow.SetActive(false);
+        }
+    }
+}
