2d5556803029dec2dbb2828b533bf30fde801ff4
kihara.k <kihara.k2000@gmail.com>
2019-04-29 15:49:01 +0900

    no message

--- Destroy/Assets/Scripts/ScoreManager.cs
+++ Destroy/Assets/Scripts/ScoreManager.cs
@@ -5,17 +5,23 @@ using UnityEngine.UI;
 public class ScoreManager : MonoBehaviour
 {
     [SerializeField]static int score;
+    [SerializeField] static float time;
     [SerializeField] Text scoreTxt;
-    public 
     // Start is called before the first frame update
     void Update()
     {
-        scoreTxt.text = score.ToString() + "円";
+        if (MainGameManaer.GetMode() != Mode.Nomal)
+        {
+            time += Time.deltaTime;
+            scoreTxt.text = time.ToString();
+        } 
+        else scoreTxt.text = score.ToString() + "円";
     }
     //スコアの初期設定
     public void ScoreSet(int sc)
     {
         score = sc;
+        time = score;
     }
     //スコアの取得
     public static int GetScore()
